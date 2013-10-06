07c5f5feb7b5514ee136499afd7b25095053bd28
neekey <ni184775761@gmail.com>
2013-10-06 17:00:48 +0800

    Initial Commit

---
+++ NewDefaults/NewDefaults.h
@@ -0,0 +1,15 @@
+//
+//  NewDefaults.h
+//  NewDefaults
+//
+//  Created by 云建 倪 on 13-10-6.
+//  Copyright (c) 2013年 com.neekey. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface NewDefaults : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ NewDefaults/NewDefaults.m
@@ -0,0 +1,17 @@
+//
+//  NewDefaults.m
+//  NewDefaults
+//
+//  Created by 云建 倪 on 13-10-6.
+//  Copyright (c) 2013年 com.neekey. All rights reserved.
+//
+
+#import "NewDefaults.h"
+
+@implementation NewDefaults
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
